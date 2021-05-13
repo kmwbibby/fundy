fde604ea70cba6bfe8f4665277f2da82f6f623d3
kbibby12 <kbibby12@gmail.com>
2021-05-13 08:26:48 -0400

    Test project commit

---
+++ src/main/java/com/kbkode/fundy/FundyApplication.java
@@ -0,0 +1,13 @@
+package com.kbkode.fundy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kbkode/fundy/FundyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbkode.fundy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
